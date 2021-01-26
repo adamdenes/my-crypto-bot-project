af3bfaf3ee74fb17e97dfc09bb4a330ee04b43ab
Adam Denes <adam.denes@icloud.com>
2021-01-26 21:00:56 +0100

    upload async helpers

---
+++ helper.js
@@ -0,0 +1,31 @@
+// async ... await GET
+const getData = async () => {
+    try {
+      const response = await fetch('https://api-to-call.com/endpoint');
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        return jsonResponse;
+      }
+      throw new Error('Request failed!');
+    } catch (error) {
+      console.log(error);
+    }
+}
+
+// async ... await POST
+const postData = async () => {
+    try {
+      const response = await fetch('https://api-to-call.com/endpoint', {
+        method: 'POST',
+        body: JSON.stringify({id: 200})
+      });
+  
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        return jsonResponse;
+      }
+      throw new Error('Request failed!');
+    } catch (error) {
+      console.log(error);
+    }
+  };
