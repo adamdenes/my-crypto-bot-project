39ec39f6c1172b09af865feda6a9cf9457a7b20b
Adam Denes <adam.denes@icloud.com>
2021-01-28 11:12:48 +0100

    add more helper text

--- helper.js
+++ helper.js
@@ -39,3 +39,47 @@ const queryString = (obj) => {
         return a 
     }, []).join( '&' );
 }
+
+/** REPRESENTATION OF ORDER OBJECT
+ * 
+ *      this.order = {
+ *           symbol: symbol,
+            side: side,
+            type: [
+                "LIMIT", 
+                "MARKET", 
+                "STOP_LOSS", 
+                "STOP_LOSS_LIMIT", 
+                "TAKE_PROFIT", 
+                "TAKE_PROFIT_LIMIT",
+                "LIMIT_MAKER"
+            ],
+            timeInForce: timeInForce,
+            quantity: quantity,
+            quoteOrderQty: quoteOrderQty,
+            price: price,
+            newClientOrderId: newClientOrderId,
+            stopPrice: stopPrice,
+            icebergQty: icebergQty,
+            newOrderRespType: ["ACK", "RESULT", "FULL"],
+            recvWindow: 0,
+            timestamp: timestamp
+        }
+ */
+
+// const operation = {
+//     _BUY: 0,
+//     _SELL: 1,
+//     BUY_DIP_THRESHOLD: 0.02, // buy if price decreased more than TH
+//     BUY_UPWARD_TREND_THRESHOLD: 0.02, // buy if price increased more than TH
+//     SELL_PROFIT_THRESHOLD: 0.02, // sell if price increased above TH
+//     SELL_STOP_LOSS_THRESHOLD: 0.02, // stop loss
+
+//     get BUY() {
+//         return this._BUY;
+//     },
+
+//     get SELL() {
+//         return this._SELL;
+//     },
+// };
