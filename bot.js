d90503d46134fa5f40c8d172b02149ecb2d5a80d
Adam Denes <adam.denes@icloud.com>
2021-02-04 08:33:27 +0100

    create server

---
+++ bot.js
@@ -0,0 +1,34 @@
+const { getData, postData, queryString } = require('./helper.js');
+const config = require('./config.json');
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 80;
+const url = `https://api.telegram.org/bot${config.telegramToken}/`
+
+const getMe = async () => {
+    const response = await getData(url + 'getMe');
+    return response;
+};
+
+const getUpdates = async () => {
+    const response = await getData(url + 'getUpdates');
+    return response.result.map(e => e);
+};
+
+// getMe().then(r => console.log(r));
+// getUpdates().then(r => console.log(r));
+
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+    // console.log(req.body);
+    const chatId = req.body.message.chat.id;
+    console.log(chatId)
+    // res.send(req.body);
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
