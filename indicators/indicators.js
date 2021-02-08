483405f056783d2e711e98c69787b82d209754b3
Adam Denes <adam.denes@icloud.com>
2021-02-08 20:17:56 +0100

    start implementing talib for indicators

---
+++ indicators/indicators.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-console */
+// Load the module and display its version
+const fs = require('fs');
+const talib = require('talib');
+const config = require('../config.json');
+const Client = require('../index');
+const { writeData } = require('../log');
+
+const client = new Client(config.apiKey, config.apiSecret);
+
+// console.log(`TALib Version: ${talib.version}`);
+
+// Retreive indicator specifications
+// const functionDesc = talib.explain('BBANDS');
+// console.dir(functionDesc);
+
+// marketData = { open: [...], close: [...], high: [...], low: [...], volume: [...] };
+const getMarketData = (json) => {
+    fs.readFile(json, (err, data) => {
+        if (err) console.error(err);
+        const content = JSON.parse(data);
+        // console.log(marketData);
+
+        // OHLCV data conversion
+        const marketData = {
+            open: Object.values(content).map((o) => o.open),
+            close: Object.values(content).map((c) => c.close),
+            high: Object.values(content).map((h) => h.high),
+            volume: Object.values(content).map((v) => v.volume),
+        };
+
+        // execute SMA indicator function with time period 180
+        talib.execute(
+            {
+                name: 'SMA',
+                startIdx: 0,
+                endIdx: marketData.close.length - 1,
+                inReal: marketData.close,
+                optInTimePeriod: 180,
+            },
+            function (err, result) {
+                if (err) console.log(err);
+                // Show the result array
+                console.log('SMA Function Results:');
+                console.log(result);
+            }
+        );
+    });
+};
+
+getMarketData('./backtest/ETHUSDT_4h.json');
