bc2d35e9a2344657f8736039425c28d884e015c5
Adam Denes <adam.denes@icloud.com>
2021-02-12 14:36:16 +0100

    testing strat

--- strategies/bollinger.js
+++ strategies/bollinger.js
@@ -1,15 +1,8 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable no-console */
-const { convertMarketData } = require('../helper');
-const { BBANDS, RSI } = require('../indicators/indicators');
-const config = require('../config.json');
-const Client = require('../index');
-
-const client = new Client(config.apiKey, config.apiSecret);
-const marketData = client.getCandlestickData('BTCUSDT', '4h').then((data) => convertMarketData(data));
 
 class MyTestStrategy {
-    constructor(dataset) {
+    constructor(dataset = {}) {
         this._dataset = dataset;
         this._indicators = [];
     }
@@ -60,15 +53,6 @@ class MyTestStrategy {
         const middleBand = bollinger.outRealMiddleBand; // .outRealMiddleBand;
         const lowerBand = bollinger.outRealLowerBand; // .outRealLowerBand;
         const rsi = relativeSI.outReal; // .outReal;
-        console.log(upperBand.slice(-1));
-        console.log(middleBand.slice(-1));
-        console.log(lowerBand.slice(-1));
-        console.log(rsi.slice(-1));
-
-        console.log(`RSI: ${rsi.slice(-1)}`);
-        console.log(`CLOSING CANDLE: ${data.close.slice(-1)}`);
-        console.log(`LOWERBAND: ${lowerBand.slice(-1)}`);
-        console.log(rsi.slice(-1) > 30 && data.close.slice(-1)[0] < lowerBand.slice(-1)[0]);
 
         return rsi.slice(-1) > 30 && data.close.slice(-1)[0] < lowerBand.slice(-1)[0];
     }
@@ -82,24 +66,8 @@ class MyTestStrategy {
         const middleBand = bollinger.outRealMiddleBand; // .outRealMiddleBand;
         const rsi = relativeSI.outReal; // .outReal;
 
-        console.log(middleBand.slice(-1));
-        console.log(rsi.slice(-1));
-
-        console.log(`RSI: ${rsi.slice(-1)}`);
-        console.log(`CLOSING CANDLE: ${data.close.slice(-1)}`);
-        console.log(`MIDDLEBAND: ${middleBand.slice(-1)}`);
-        console.log(rsi.slice(-1)[0] > 80 && data.close.slice(-1)[0] > middleBand.slice(-1)[0]);
-
         return rsi.slice(-1)[0] > 70 && data.close.slice(-1)[0] > middleBand.slice(-1)[0];
     }
 }
 
-const strat = new MyTestStrategy();
-
-marketData.then((d) => {
-    strat.dataset = d;
-    strat.populateIndicators(BBANDS(d));
-    strat.populateIndicators(RSI(d));
-    strat.generateBuySignal();
-    strat.generateSellSignal();
-});
+module.exports = MyTestStrategy;
